b1dc36e67129699e7a2c3f7da45d7e71084ff311
Boss <57038976+boss-error@users.noreply.github.com>
2025-04-24 02:48:12 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,31 +1,12 @@
 #!/bin/sh
 
-# Update package list and install gdebi
-echo "Installing gdebi..."
-sudo apt update
-sudo apt install -y gdebi
 
-# Define URLs
-ADD_APT_KEY_URL="https://github.com/gaining/Resetter/releases/download/v3.0.0-stable/add-apt-key_1.0-0.5_all.deb"
-RESETTER_URL="https://github.com/gaining/Resetter/releases/download/v3.0.0-stable/resetter_3.0.0-stable_all.deb"
+https://github.com/gaining/Resetter/releases/download/v3.0.0-stable/add-apt-key_1.0-0.5_all.deb
 
-# Download add-apt-key
-echo "Downloading add-apt-key..."
-wget "$ADD_APT_KEY_URL" -O add-apt-key.deb
 
-# Install add-apt-key
-echo "Installing add-apt-key..."
-sudo gdebi add-apt-key.deb
+sudo dpkg -i add-apt-key_1.0-0.5_all.deb
 
-# Download Resetter
-echo "Downloading Resetter..."
-wget "$RESETTER_URL" -O resetter.deb
+wget https://github.com/gaining/Resetter/releases/download/v3.0.0-stable/resetter_3.0.0-stable_all.deb
+sudo dpkg -i resetter_3.0.0-stable_all.deb
 
-# Install Resetter
-echo "Installing Resetter..."
-sudo gdebi resetter.deb
-
-# Clean up
-rm add-apt-key.deb resetter.deb
-
-echo "Resetter installed successfully."
+sudo resetter
